9a9bde7181f17d6750ad36a5e1a91eb551c6a74d
Petros Stergioulas <petros.stergioulas94@gmail.com>
2020-09-06 20:26:21 +0200

    Add Gateway

--- customer-service/src/main/java/de/stergioulas/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/de/stergioulas/customer/CustomerServiceApplication.java
@@ -1,8 +1,14 @@
 package de.stergioulas.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +18,15 @@ public class CustomerServiceApplication {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+    @RestController
+    class CustomerController {
+
+        @Autowired
+        Environment env;
+
+        @GetMapping("/customers")
+        List<String> customers() {
+            return List.of("Petros", env.getProperty("server.port"));
+        }
+    }
 }
