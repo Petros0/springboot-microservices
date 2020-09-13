2a4a4e8d5be9ba064e953cf7417d5ec74a7dd93c
Petros Stergioulas <petros.stergioulas94@gmail.com>
2020-09-13 16:09:48 +0200

    Add RabbitMQ

--- address-service/src/main/java/de/stergioulas/address/AddressServiceApplication.java
+++ address-service/src/main/java/de/stergioulas/address/AddressServiceApplication.java
@@ -1,11 +1,19 @@
 package de.stergioulas.address;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +39,41 @@ public class AddressServiceApplication {
 		List<String> customers() {
 			log.info("Addresses endpoint ->");
 			return List.of("450  Leisure Lane", "1673  Gerald L. Bates Drive");
+		}
+	}
+
+	@Component
+	@Profile("rabbit")
+	public class RabbitMqListener {
+
+		private String queueName = "my-queue";
+		private String exchange = "my-exchange";
+		private String routingKey = "#routing.key";
+
+
+		@RabbitListener(queues = "my-queue")
+		public void listen(String message) {
+			String msg = new String(message);
+			System.out.println("Received a new message...");
+			System.out.println(msg);
+		}
+
+		@Bean
+		Queue queue() {
+			// Creating a queue.
+			return new Queue(queueName, false);
+		}
+
+		@Bean
+		TopicExchange topicExchange() {
+			// Creating a topic exchange.
+			return new TopicExchange(exchange);
+		}
+
+		@Bean
+		Binding binding(final Queue queue, final TopicExchange topicExchange) {
+			// Binding the queue to the topic with a routing key.
+			return BindingBuilder.bind(queue).to(topicExchange).with(routingKey);
 		}
 	}
 
--- customer-service/src/main/java/de/stergioulas/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/de/stergioulas/customer/CustomerServiceApplication.java
@@ -2,6 +2,13 @@ package de.stergioulas.customer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -9,7 +16,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,7 +31,15 @@ import java.util.List;
 @EnableEurekaClient
 @EnableFeignClients
 @Log4j2
-public class CustomerServiceApplication {
+@RequiredArgsConstructor
+public class CustomerServiceApplication implements CommandLineRunner {
+
+
+    @Autowired
+    Environment env;
+
+    @Autowired
+    Binding binding;
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
@@ -34,6 +51,15 @@ public class CustomerServiceApplication {
         return new RestTemplate();
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (env.acceptsProfiles(Profiles.of("rabbit"))) {
+
+        }
+    }
+
+
     @FeignClient("address-service")
     interface AddressClient {
         @RequestMapping(method = RequestMethod.GET, value = "/addresses")
@@ -44,20 +70,63 @@ public class CustomerServiceApplication {
     @RequiredArgsConstructor
     class CustomerController {
 
-        private final Environment env;
         private final RestTemplate restTemplate;
         private final AddressClient client;
 
+        // simple endpoint
         @GetMapping("/customers")
         List<String> customers() {
             log.info("Customers endpoint ->");
             return List.of("Nyah", "Rhea");
         }
 
+
         @GetMapping("/customers/addresses")
         Object addresses() {
             log.info("Addresses endpoint ->");
             return client.getAddresses();
+        }
+
+// without feign
+//        @GetMapping("/customers/addresses")
+//        Object addresses() {
+//            log.info("Addresses endpoint ->");
+//            return restTemplate.getForObject("http://address-service/addresses", Object.class) without feign
+//
+//        }
+    }
+
+    @RequiredArgsConstructor
+    @Profile("rabbit")
+    @RestController
+    class RabbitConfig {
+
+        private final RabbitTemplate rabbitTemplate;
+        private String queueName = "my-queue";
+        private String exchange = "my-exchange";
+        private String routingKey = "#routing.key";
+
+        @Bean
+        Queue queue() {
+            // Creating a queue.
+            return new Queue(queueName, false);
+        }
+
+        @Bean
+        TopicExchange topicExchange() {
+            // Creating a topic exchange.
+            return new TopicExchange(exchange);
+        }
+
+        @Bean
+        Binding binding(final Queue queue, final TopicExchange topicExchange) {
+            // Binding the queue to the topic with a routing key.
+            return BindingBuilder.bind(queue).to(topicExchange).with(routingKey);
+        }
+
+        @GetMapping("/customers/send")
+        public void sendToRabbit() {
+            rabbitTemplate.convertAndSend(exchange, routingKey, "message");
         }
     }
 }
